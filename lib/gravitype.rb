308a0b8bdd8eb71f74b9c8c96a1b8202f56eafb8
Eloy Durán <eloy.de.enige@gmail.com>
2016-12-22 21:09:40 -0500

    Introspect data from mongoid

--- lib/gravitype.rb
+++ lib/gravitype.rb
@@ -1,5 +1,53 @@
-require "gravitype/version"
+require 'set'
 
 module Gravitype
-  # Your code goes here...
+  class List < ::Set
+    def self.for_list(list)
+      case list
+      when ::Array then Array.new
+      when ::Set then Set.new
+      end
+    end
+
+    def +(other)
+      raise TypeError, 'Conflicting list types' if other.class != self.class
+      super
+    end
+
+    def <<(klass)
+      raise TypeError, 'Unexpected nested list type' if [Array, Set, Hash].include?(klass)
+      super
+    end
+
+    class Array < List
+      alias_method :to_list, :to_a
+    end
+
+    class Set < List
+      alias_method :to_list, :to_set
+    end
+  end
+
+  def self.introspect_data(model, fields_with_getters)
+    fields_with_classes = Hash.new { |h,k| h[k] = Set.new }
+    model.all.each do |doc|
+      fields_with_getters.each do |field, getter|
+        value = doc.send(getter)
+        if list = List.for_list(value)
+          classes = value.inject(list) { |list, element| list << element.class }
+          fields_with_classes[field] << classes
+        else
+          fields_with_classes[field] << value.class
+        end
+      end
+    end
+    fields_with_classes.each do |field, classes|
+      lists = classes.select { |x| x.is_a?(List) }
+      unless lists.empty?
+        classes.delete_if { |x| x.is_a?(List) }
+        classes << lists.reduce(:+).to_list
+      end
+    end
+    fields_with_classes
+  end
 end
--- test/gravitype_test.rb
+++ test/gravitype_test.rb
@@ -8,10 +8,88 @@ class TestDoc
   end
 end
 
-class GravitypeTest < Minitest::Spec
-  it 'works' do
-    doc = TestDoc.new(mongoid_string: 'hello')
-    doc.save!
-    assert doc
+# mongoid_hash
+
+describe 'Data inflection of Mongoid backed fields' do
+  it 'returns all types that exist for a `string` field' do
+    result = introspect_field(:mongoid_string, [nil, 'foo', 'bar'])
+    result.must_equal Set.new([NilClass, String])
+  end
+
+  it 'returns all types that exist for a `symbol` field' do
+    result = introspect_field(:mongoid_symbol, [nil, :foo, :bar])
+    result.must_equal Set.new([NilClass, Symbol])
+  end
+
+  it 'returns all types that exist for a `boolean` field' do
+    result = introspect_field(:mongoid_boolean, [nil, true, true, false])
+    result.must_equal Set.new([NilClass, TrueClass, FalseClass])
+  end
+
+  it 'returns all types that exist for a `integer` field' do
+    result = introspect_field(:mongoid_integer, [nil, 21, 42])
+    result.must_equal Set.new([NilClass, Fixnum])
+  end
+
+  it 'returns all types that exist for a `float` field' do
+    result = introspect_field(:mongoid_float, [nil, 21.0, 42.0])
+    result.must_equal Set.new([NilClass, Float])
+  end
+
+  it 'returns all types that exist for a `big_decimal` field' do
+    result = introspect_field(:mongoid_float, [nil, BigDecimal.new('42'), BigDecimal.new('42424242424242424242424.42')])
+    result.must_equal Set.new([NilClass, Float])
+  end
+
+  it 'returns all types that exist for a `time` field' do
+    result = introspect_field(:mongoid_time, [nil, Time.now, Time.now, 'foo'])
+    result.must_equal Set.new([NilClass, Time])
+  end
+
+  it 'returns all types that exist for a `date` field' do
+    result = introspect_field(:mongoid_date, [nil, Date.today, Date.today])
+    result.must_equal Set.new([NilClass, Date])
+  end
+
+  it 'returns all types that exist for a `datetime` field' do
+    result = introspect_field(:mongoid_date_time, [nil, DateTime.now, DateTime.now])
+    result.must_equal Set.new([NilClass, DateTime])
+  end
+
+  it 'returns all types that exist for a `object_id` field' do
+    result = introspect_field(:mongoid_object_id, [nil, BSON::ObjectId.new.to_s, BSON::ObjectId.new])
+    result.must_equal Set.new([NilClass, BSON::ObjectId])
+  end
+
+  it 'returns all types that exist for a `binary` field' do
+    result = introspect_field(:mongoid_binary, [nil, 'baz', BSON::Binary.new('foo'), BSON::Binary.new('bar')])
+    result.must_equal Set.new([NilClass, BSON::Binary, String])
+  end
+
+  it 'returns all types that exist for a `range` field' do
+    result = introspect_field(:mongoid_range, [nil, 21..42, 21...42])
+    result.must_equal Set.new([NilClass, Range])
+  end
+
+  it 'returns all types that exist for a `regexp` field' do
+    result = introspect_field(:mongoid_regexp, ['foo', /foo/, /bar/])
+    result.must_equal Set.new([BSON::Regexp::Raw])
+  end
+
+  it 'returns all types that exist for an `array` field' do
+    result = introspect_field(:mongoid_array, [nil, [], ['foo', :bar], [42]])
+    result.must_equal Set.new([NilClass, [String, Symbol, Fixnum]])
+  end
+
+  it 'returns all types that exist for a `set` field' do
+    result = introspect_field(:mongoid_set, [nil, Set.new, Set.new(['foo', :bar]), Set.new([42])])
+    result.must_equal Set.new([Set.new([String, Symbol, Fixnum])])
+  end
+
+  private
+
+  def introspect_field(field, values)
+    values.each { |value| TestDoc.create!(field => value) }
+    Gravitype.introspect_data(TestDoc, field => field)[field]
   end
 end

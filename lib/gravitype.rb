5770d287fe1cecdf78d8ccd64960fabc711edce9
Eloy Durán <eloy.de.enige@gmail.com>
2017-01-27 22:35:35 -0500

    Add high-level entry points.

--- lib/gravitype.rb
+++ lib/gravitype.rb
@@ -2,6 +2,16 @@ require "gravitype/introspection/data"
 require "gravitype/introspection/schema"
 
 module Gravitype
+  def self.introspect(*models)
+    if models.empty?
+      models = Mongoid.models.select { |model| model.try(:cached_json_field_defs) }
+    end
+    models.inject({}) do |hash, model|
+      hash[model.name] = Model.new(model).introspect
+      hash
+    end
+  end
+
   class Model
     def initialize(model)
       @model = model

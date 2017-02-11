074a56a0e98809935c32eb0a5d4169332a515d1b
Eloy Durán <eloy.de.enige@gmail.com>
2017-02-11 14:25:29 +0100

    Cleanup.

--- test/introspection_test.rb
+++ test/introspection_test.rb
@@ -1,14 +1,5 @@
 require "test_helper"
 
-describe Gravitype::Introspection do
-  # it "returns a list of exposed JSON fields and their model getters" do
-  #   Gravitype::Introspection.new(TestDoc).exposed_fields_and_getters.must_equal({
-  #     mongoid_string: :mongoid_string,
-  #     ruby_method: :ruby_method?,
-  #   })
-  # end
-end
-
 module Gravitype
   describe Introspection::Model do
     include Type::DSL
